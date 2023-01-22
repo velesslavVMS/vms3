13cca775d0b759f6b974a5987d76a6608865d33f
Vedamir <grandros1@gmail.com>
2023-01-22 14:11:16 +0800

    Task 23

---
+++ task23/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i += 1) // (begin, condition, step)
+Console.Write($"{Math.Pow(i, 3)}  ");
