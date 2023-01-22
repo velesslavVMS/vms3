ab958d021eff524674ae6a4d71bb7495a10f8610
Vedamir <grandros1@gmail.com>
2023-01-22 13:57:08 +0800

    Task 21

---
+++ task21/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Введите координату X первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z первой точки: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату X второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z второй точки: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double S = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y1 - y2, 2)+ Math.Pow(z1 - z2, 2)), 2); 
+Console.WriteLine($"Расстояние равно: {S}");
